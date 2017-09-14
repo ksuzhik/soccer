f36adb81d9d50d8858b9517e4924207a1e347501
Kseniya Timofeeva <kseniya.timofeeva@yalantis.com>
2017-09-14 22:10:38 +0300

    readme; handle exceptions

---
+++ README.md
@@ -0,0 +1,11 @@
+The rules
+---------
+In this league, a draw (tie) is worth 1 point and a win is worth 3 points. A
+loss is worth 0 points. If two or more teams have the same number of points,
+they should have the same rank and be printed in alphabetical order (as in the
+tie for 3rd place in the sample data).
+
+Commands:
+---------
+rake task - for running test
+ruby run.rb - for running script
--- run.rb
+++ run.rb
@@ -10,8 +10,18 @@ require './lib/soccer/league'
 def calculate_results(input_data)
   parsed_data = Parser.process(input_data)
   Formatter.process Soccer::League.new(parsed_data).results
+rescue Exception => e
+  puts "Can't calculate results."
+  exit
 end
 
-input_data = FileProcessor.read_lines(ARGV[0])
+def fetch_input_data(filename)
+  FileProcessor.read_lines(filename)
+rescue Exception => e
+  puts "Can't read file #{ARGV[0]}. Please provile correct file path."
+  exit
+end
+
+input_data = fetch_input_data(ARGV[0])
 puts 'Results:'
 puts calculate_results(input_data)
